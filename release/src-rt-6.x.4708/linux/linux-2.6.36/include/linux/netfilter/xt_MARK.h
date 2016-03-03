24c82842ee2f179ff718e779fb2b246d1776c62b
Shibby <shibby@openlinksys.info>
2016-03-03 10:01:37 +0100

    fix MARK module
    revert previous version.

--- release/src-rt-6.x.4708/linux/linux-2.6.36/include/linux/netfilter/xt_MARK.h
+++ release/src-rt-6.x.4708/linux/linux-2.6.36/include/linux/netfilter/xt_MARK.h
@@ -1,24 +1,6 @@
 #ifndef _XT_MARK_H_target
 #define _XT_MARK_H_target
 
-/* Version 0 */
-struct xt_mark_target_info {
-	unsigned long mark;
-};
-
-/* Version 1 */
-enum {
-	XT_MARK_SET=0,
-	XT_MARK_AND,
-	XT_MARK_OR,
-	XT_MARK_SET_RETURN,
-	XT_MARK_AND_RETURN,
-	XT_MARK_OR_RETURN,
-};
-
-struct xt_mark_target_info_v1 {
-	unsigned long mark;
-	u_int8_t mode;
-};
+#include <linux/netfilter/xt_mark.h>
 
 #endif /*_XT_MARK_H_target */
