3c204a8184b858c8144d0dd995a41eb917e10b76
Jacky Prahec <jacky@prahec.com>
2017-09-19 03:40:04 +0200

    Fix: https://github.com/Jackysi/advancedtomato/issues/348

--- release/src-rt-6.x.4708/router/shared/defaults.c
+++ release/src-rt-6.x.4708/router/shared/defaults.c
@@ -276,7 +276,7 @@ struct nvram_tuple router_defaults[] = {
 	{ "ipv6_pdonly",		"0"				, 0 },	// Request DHCPv6 Prefix Delegation Only
 	{ "ipv6_ipsec",			"1"				, 0 },	// Enable Incoming IPv6 IPSec
 	{ "ipv6_wan_addr",		""				, 0 },	// Static IPv6 WAN Address
-	{ "ipv6_prefix_len_wan",""				, 0 },	// Static IPv6 WAN Prefix Length
+	{ "ipv6_prefix_len_wan","64"			, 0 },	// Static IPv6 WAN Prefix Length
 	{ "ipv6_isp_gw",		""				, 0 },	// Static IPv6 ISP Gateway
 #endif
 
